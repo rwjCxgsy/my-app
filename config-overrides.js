40650c48db25300fa9fbc9886d7ff47069534b2a
DESKTOP-D2NIS90\rwj84 <847679857@qq.com>
2019-06-08 21:48:10 +0800

    更改webpack配置

---
+++ config-overrides.js
@@ -0,0 +1,36 @@
+const { override, fixBabelImports, addLessLoader, addWebpackAlias, addPostcssPlugins } = require('customize-cra');
+const path = require('path')
+
+function resolve(dir) {
+    return path.join(__dirname, dir)
+}
+console.log('使用新编译')
+
+module.exports = override(
+    addWebpackAlias({
+        '@': resolve('src')
+    }),
+    fixBabelImports('import', {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true,
+    }),
+    addLessLoader({
+        javascriptEnabled: true,
+        modifyVars: { '@primary-color': '#1A73E8' },
+        localIdentName: '[local]--[hash:base64:5]'
+    }),
+    addPostcssPlugins([
+        require('autoprefixer')({
+            flexbox: true
+        }),
+        require('postcss-preset-env'),
+        require("postcss-write-svg"),
+        require('postcss-cssnext')({
+            mainColor: "#1A73E8",
+            altColor: "red"
+        }),
+        require('postcss-import'),
+        require('postcss-url'),
+    ]),
+);
